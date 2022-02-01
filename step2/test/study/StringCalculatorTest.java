0399584487d89ac0c5f04eec87cfef83279b8f8e
sungsanghyun <sos9210@naver.com>
2022-02-01 16:44:29 +0900

    리팩토링 22/02/01

--- step2/test/study/StringCalculatorTest.java
+++ step2/test/study/StringCalculatorTest.java
@@ -12,7 +12,33 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void add() {
+    public void add_null_또는_빈문자() {
+        Assert.assertEquals(0,cal.sum(null));
+        Assert.assertEquals(0,cal.sum(""));
+    }
+    @Test
+    public void add_숫자하나() {
+        Assert.assertEquals(1,cal.sum("1"));
+    }
+    @Test
+    public void add_쉼표구분자() {
+        Assert.assertEquals(3,cal.sum("1,2"));
+    }
+    @Test
+    public void add_쉼표_또는_콜론_구분자() {
+        Assert.assertEquals(6,cal.sum("1,2:3"));
+    }
+    @Test
+    public void add_custom_구분자() {
+        Assert.assertEquals(6,cal.sum("//;\n1;2;3"));
+    }
+    @Test(expected = RuntimeException.class)
+    public void add_negative() {
+        cal.sum("-1,2,3");
+    }
+
+/*    @Test
+    public void sum() {
         Assert.assertEquals(6,cal.sum("1:2,3"));
         Assert.assertEquals(6,cal.sum("//;\n1;2;3"));
     }
@@ -33,5 +59,6 @@ public class StringCalculatorTest {
     public void divide() {
         Assert.assertEquals(1,cal.divide("12:4,3"));
         Assert.assertEquals(1,cal.divide("//;\n12;4;3"));
-    }
+    }*/
+
 }
