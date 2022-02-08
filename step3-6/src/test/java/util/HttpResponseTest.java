d705c9abec0ffeb9bcf3fe8281fc403bfc55d3da
sungsanghyun <sos9215@gmail.com>
2022-02-09 00:08:10 +0900

    HttpRequest, RequestHandler 리팩토링 수정 HttpResponse클래스 추가 HttpResponseTest 작성예정

---
+++ step3-6/src/test/java/util/HttpResponseTest.java
@@ -0,0 +1,11 @@
+package util;
+
+import org.junit.Test;
+
+public class HttpResponseTest {
+    private String testDirectory = "./src/test/resources/";
+
+    @Test
+    public void responseForward() throws Exception {
+    }
+}
