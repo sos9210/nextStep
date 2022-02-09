254532677efd24fe8e8ca65c410fa4ff37dd248c
sungsanghyun <sos9210@naver.com>
2022-02-10 01:08:56 +0900

    HttpResponseTest.java 추가 RequestHandler 클래스 HttpResponse적용 리팩토링

--- step3-6/src/test/java/util/HttpResponseTest.java
+++ step3-6/src/test/java/util/HttpResponseTest.java
@@ -1,11 +1,36 @@
 package util;
 
+import http.HttpResponse;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 public class HttpResponseTest {
-    private String testDirectory = "./src/test/resources/";
 
     @Test
     public void responseForward() throws Exception {
+        //index.html과 같은내용의 txt파일생성
+        HttpResponse response = new HttpResponse(createOutputStream("Http_Forward.txt"));
+        response.forward("/index.html");
+    }
+    @Test
+    public void responseDirect() throws Exception {
+        //302응답 Location: /index.html인 파일생성
+        HttpResponse response = new HttpResponse(createOutputStream("Http_Redirect.txt"));
+        response.sendRedirect("/index.html");
+    }
+
+    @Test
+    public void responseCookies() throws Exception{
+        // 응답헤더에 Set-Cookie: logined=true인 파일생성
+        HttpResponse response = new HttpResponse(createOutputStream("Http_Cookie.txt"));
+        response.addHeader("Set-Cookie","logined=true");
+        response.sendRedirect("/index.html");
+    }
+    private OutputStream createOutputStream(String filename) throws FileNotFoundException {
+        return new FileOutputStream(new File("./src/test/resources/"+filename));
     }
 }
