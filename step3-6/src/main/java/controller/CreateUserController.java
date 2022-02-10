3d6bc9245d76e2e37be6b1866fd8d9512fdc011a
sungsanghyun <sos9210@naver.com>
2022-02-10 23:50:27 +0900

    컨트롤러 추가

---
+++ step3-6/src/main/java/controller/CreateUserController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import db.DataBase;
+import http.HttpRequest;
+import http.HttpResponse;
+import model.User;
+
+public class CreateUserController extends AbstractController{
+
+    @Override
+    public void doPost(HttpRequest request, HttpResponse response) {
+        User user =  new User(request.getParameter("userId"), request.getParameter("password"),
+                request.getParameter("name"),request.getParameter("email"));
+        DataBase.addUser(user);
+        response.sendRedirect("/index.html");
+    }
+}
--- step3-6/src/main/java/webserver/RequestHandler.java
+++ step3-6/src/main/java/webserver/RequestHandler.java
@@ -5,10 +5,11 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import controller.Controller;
+import controller.*;
 import db.DataBase;
 import http.HttpResponse;
 import model.User;
@@ -19,11 +20,17 @@ import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-
+    private Map<String,Controller> controllerMap = new HashMap<>();
     private Socket connection;
     private Controller controller;
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+    }
+
+    private void init() {
+        controllerMap.put("/user/create", new CreateUserController());
+        controllerMap.put("/user/login", new LoginController());
+        controllerMap.put("/user/list.html", new ListUserController());
     }
 
     public void run() {
@@ -32,217 +39,18 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            init();
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
-
-            if(request.getPath().equals("/user/create")) {
-                User user =  new User(request.getParameter("userId"), request.getParameter("password"),
-                                request.getParameter("name"),request.getParameter("email"));
-                DataBase.addUser(user);
-                response.sendRedirect("/index.html");
-            }else if(request.getPath().equals("/user/login")) {
-                User loginUser = new User(request.getParameter("userId"), request.getParameter("password"),
-                                request.getParameter("name"),request.getParameter("email"));
-                User findUser = DataBase.findUserById(loginUser.getUserId());
-                if (findUser != null && loginUser.getPassword().equals(findUser.getPassword())) {
-                    response.addHeader("Set-Cookie","logined=true");
-                    response.sendRedirect("/index.html");
-                    log.debug("success login");
-                } else {
-                    response.addHeader("Set-Cookie","logined=false");
-                    response.sendRedirect("/index.html");
-                    log.debug("fail login");
-                }
-            }else if(request.getPath().equals("/user/list.html")) {
-                Map<String, String> cookie = HttpRequestUtils.parseCookies(request.getHeader("Cookie"));
-                if (!cookie.getOrDefault("logined", "false").equals("true")) {
-                    response.sendRedirect("/index.html");
-                } else {
-                    Collection<User> all = DataBase.findAll();      //모든 회원 조회
-                    StringBuilder createListHtml = listHtml(all);   //html 동적 생성
-                    response.forwardBody(createListHtml.toString());
-                }
-            }else{
+            log.debug("method ..... {}",request.getMethod());
+            Controller controller = controllerMap.get(request.getPath());
+            if(controller == null){
                 response.forward(request.getPath());
             }
+            controller.service(request,response);
         } catch (Exception e) {
             log.error(e.getMessage());
             e.printStackTrace();
         }
-    }
-/*
-    private void responseLoginHeader(DataOutputStream dos, boolean loginYn) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found\r\n");
-            dos.writeBytes("Location: /index.html" + "\r\n");
-            dos.writeBytes("Set-Cookie: logined="+loginYn+"\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error("header200 error ... {}",e.getMessage());
-            e.printStackTrace();
-            log.error("header200 error ... {}",e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error("header200 error ... {}",e.getMessage());
-            e.printStackTrace();
-            log.error("header200 error ... {}",e.getMessage());
-        }
-    }
-    private void responseCssHeader(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error("css200 error ... {}",e.getMessage());
-            e.printStackTrace();
-            log.error("css200 error ... {}",e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found\r\n");
-            dos.writeBytes("Location: /index.html" + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error("header302 error ... {}",e.getMessage());
-            e.printStackTrace();
-            log.error("header302 error ... {}",e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error("body error ... {}",e.getMessage());
-            e.printStackTrace();
-            log.error("body error ... {}",e.getMessage());
-        }
-    }
-*/
-
-    private StringBuilder listHtml(Collection<User> users){
-        StringBuilder html = new StringBuilder();
-        html.append("<!DOCTYPE html>\n" +
-                "<html lang=\"kr\">\n" +
-                "<head>\n" +
-                "    <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n" +
-                "    <meta charset=\"utf-8\">\n" +
-                "    <title>SLiPP Java Web Programming</title>\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\">\n" +
-                "    <link href=\"../css/bootstrap.min.css\" rel=\"stylesheet\">\n" +
-                "    <!--[if lt IE 9]>\n" +
-                "    <script src=\"//html5shim.googlecode.com/svn/trunk/html5.js\"></script>\n" +
-                "    <![endif]-->\n" +
-                "    <link href=\"../css/styles.css\" rel=\"stylesheet\">\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<nav class=\"navbar navbar-fixed-top header\">\n" +
-                "    <div class=\"col-md-12\">\n" +
-                "        <div class=\"navbar-header\">\n" +
-                "\n" +
-                "            <a href=\"../index.html\" class=\"navbar-brand\">SLiPP</a>\n" +
-                "            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar-collapse1\">\n" +
-                "                <i class=\"glyphicon glyphicon-search\"></i>\n" +
-                "            </button>\n" +
-                "\n" +
-                "        </div>\n" +
-                "        <div class=\"collapse navbar-collapse\" id=\"navbar-collapse1\">\n" +
-                "            <form class=\"navbar-form pull-left\">\n" +
-                "                <div class=\"input-group\" style=\"max-width:470px;\">\n" +
-                "                    <input type=\"text\" class=\"form-control\" placeholder=\"Search\" name=\"srch-term\" id=\"srch-term\">\n" +
-                "                    <div class=\"input-group-btn\">\n" +
-                "                        <button class=\"btn btn-default btn-primary\" type=\"submit\"><i class=\"glyphicon glyphicon-search\"></i></button>\n" +
-                "                    </div>\n" +
-                "                </div>\n" +
-                "            </form>\n" +
-                "            <ul class=\"nav navbar-nav navbar-right\">\n" +
-                "                <li>\n" +
-                "                    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"glyphicon glyphicon-bell\"></i></a>\n" +
-                "                    <ul class=\"dropdown-menu\">\n" +
-                "                        <li><a href=\"https://slipp.net\" target=\"_blank\">SLiPP</a></li>\n" +
-                "                        <li><a href=\"https://facebook.com\" target=\"_blank\">Facebook</a></li>\n" +
-                "                    </ul>\n" +
-                "                </li>\n" +
-                "                <li><a href=\"../user/list.html\"><i class=\"glyphicon glyphicon-user\"></i></a></li>\n" +
-                "            </ul>\n" +
-                "        </div>\n" +
-                "    </div>\n" +
-                "</nav>\n" +
-                "<div class=\"navbar navbar-default\" id=\"subnav\">\n" +
-                "    <div class=\"col-md-12\">\n" +
-                "        <div class=\"navbar-header\">\n" +
-                "            <a href=\"#\" style=\"margin-left:15px;\" class=\"navbar-btn btn btn-default btn-plus dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"glyphicon glyphicon-home\" style=\"color:#dd1111;\"></i> Home <small><i class=\"glyphicon glyphicon-chevron-down\"></i></small></a>\n" +
-                "            <ul class=\"nav dropdown-menu\">\n" +
-                "                <li><a href=\"../user/profile.html\"><i class=\"glyphicon glyphicon-user\" style=\"color:#1111dd;\"></i> Profile</a></li>\n" +
-                "                <li class=\"nav-divider\"></li>\n" +
-                "                <li><a href=\"#\"><i class=\"glyphicon glyphicon-cog\" style=\"color:#dd1111;\"></i> Settings</a></li>\n" +
-                "            </ul>\n" +
-                "            \n" +
-                "            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar-collapse2\">\n" +
-                "            \t<span class=\"sr-only\">Toggle navigation</span>\n" +
-                "            \t<span class=\"icon-bar\"></span>\n" +
-                "            \t<span class=\"icon-bar\"></span>\n" +
-                "            \t<span class=\"icon-bar\"></span>\n" +
-                "            </button>            \n" +
-                "        </div>\n" +
-                "        <div class=\"collapse navbar-collapse\" id=\"navbar-collapse2\">\n" +
-                "            <ul class=\"nav navbar-nav navbar-right\">\n" +
-                "                <li class=\"active\"><a href=\"../index.html\">Posts</a></li>\n" +
-                "                <li><a href=\"../user/login.html\" role=\"button\">로그인</a></li>\n" +
-                "                <li><a href=\"../user/form.html\" role=\"button\">회원가입</a></li>\n" +
-                "                <li><a href=\"#\" role=\"button\">로그아웃</a></li>\n" +
-                "                <li><a href=\"#\" role=\"button\">개인정보수정</a></li>\n" +
-                "            </ul>\n" +
-                "        </div>\n" +
-                "    </div>\n" +
-                "</div>\n" +
-                "\n" +
-                "<div class=\"container\" id=\"main\">\n" +
-                "   <div class=\"col-md-10 col-md-offset-1\">\n" +
-                "      <div class=\"panel panel-default\">\n" +
-                "          <table class=\"table table-hover\">\n" +
-                "              <thead>\n" +
-                "                <tr>\n" +
-                "                    <th>#</th> <th>사용자 아이디</th> <th>이름</th> <th>이메일</th><th></th>\n" +
-                "                </tr>\n" +
-                "              </thead>\n" +
-                "              <tbody>\n");
-                if(users.isEmpty()){
-                    html.append("<tr><td>유저 정보가 없습니다.</td></tr>");
-                }else{
-                    for(User list: users){
-                        int i = 1;
-                        html.append(
-                "                <tr>\n" +
-                "                    <th scope=\"row\">"+(i++)+"</th> <td>"+list.getName()+"</td> <td>"+list.getUserId()+"</td> <td>"+list.getEmail()+"</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n" +
-                "                </tr>\n");
-                    }
-                }
-                html.append("              </tbody>\n" +
-                "          </table>\n" +
-                "        </div>\n" +
-                "    </div>\n" +
-                "</div>\n" +
-                "\n" +
-                "<!-- script references -->\n" +
-                "<script src=\"../js/jquery-2.2.0.min.js\"></script>\n" +
-                "<script src=\"../js/bootstrap.min.js\"></script>\n" +
-                "<script src=\"../js/scripts.js\"></script>\n" +
-                "\t</body>\n" +
-                "</html>");
-                return html;
     }
 }
