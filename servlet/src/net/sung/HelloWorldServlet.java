9177510a9ba1cfb2a4e5482506ca535d297ce8c2
sungsanghyun <sos9215@gmail.com>
2022-02-15 00:21:02 +0900

    서블릿 컨테이너, 서블릿/jsp

---
+++ servlet/src/net/sung/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package net.sung;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("Hello servlet world");
+    }
+}
