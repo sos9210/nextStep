67ba839c0f836aecc6532d6c120328deb24ccefd
sungsanghyun <sos9210@naver.com>
2022-02-20 20:03:50 +0900

    서블릿 -> 컨트롤러 변경작업 및 책을보고 보완할 부분 수정

--- mvc1/src/main/java/next/front/RequestMapping.java
+++ mvc1/src/main/java/next/front/RequestMapping.java
@@ -1,18 +1,38 @@
 package next.front;
 
-import javassist.NotFoundException;
-import next.controller.Controller;
-import next.controller.CreateUserGetController;
+import next.controller.*;
+import org.h2.command.dml.Update;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RequestMapping {
+    private final Logger log = LoggerFactory.getLogger(getClass());
     Map<String, Controller> controllerMap = new HashMap<>();
 
     public void init(){
-        controllerMap.put("/users/create",new CreateUserGetController());
-        controllerMap.put("/users/form",new CreateUserGetController());
+        controllerMap.put("/",new HomeController());
+        controllerMap.put("/users/form",new FowardController("/user/form.jsp"));
+        controllerMap.put("/users/loginForm",new FowardController("/user/login.jsp"));
+        controllerMap.put("/users", new ListUserController());
+        controllerMap.put("/users/login", new LoginController());
+        controllerMap.put("/users/profile", new ProfileController());
+        controllerMap.put("/users/logout", new LogoutController());
+        controllerMap.put("/users/create",new CreateUserController());
+        controllerMap.put("/users/updateForm", new UpdateFromUserController());
+        controllerMap.put("/users/update", new UpdateUserController());
+
+        log.debug("Initialized Request Mapping");
+    }
+
+    public Controller findController(String url){
+        return controllerMap.get(url);
+    }
+
+    void put(String url, Controller controller){
+        controllerMap.put(url,controller);
     }
 
     public Controller getController(String url){
