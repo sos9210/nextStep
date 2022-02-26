5d3558fe682b1b97cc9d88a833536ee4bd129222
sungsanghyun <sos9210@naver.com>
2022-02-27 01:37:37 +0900

    UserDAO JDBC 실습 작성 수정 - delete테스트 추가 , DataAccessException 클래스 추가

--- db/src/main/java/next/dao/PreparedStatementSetter.java
+++ db/src/main/java/next/dao/PreparedStatementSetter.java
@@ -3,6 +3,7 @@ package next.dao;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementSetter {
     void setValue(PreparedStatement pstmt) throws SQLException;
 }
