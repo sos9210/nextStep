607346d79e37845a0ae5355bd8ecc2bf96b6b8a9
sungsanghyun <sos9215@gmail.com>
2022-03-05 01:17:33 +0900

    ModelAndView , View 추가 리팩토링

--- board/src/main/java/next/controller/ListUserController.java
+++ board/src/main/java/next/controller/ListUserController.java
@@ -3,18 +3,21 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import core.mvc.AbstractController;
 import core.mvc.Controller;
+import core.mvc.JspView;
+import core.mvc.ModelAndView;
 import next.dao.UserDao;
 
-public class ListUserController implements Controller {
+public class ListUserController extends AbstractController {
     @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         if (!UserSessionUtils.isLogined(req.getSession())) {
-            return "redirect:/users/loginForm";
+            return jspView("redirect:/users/loginForm");
         }
 
         UserDao userDao = new UserDao();
         req.setAttribute("users", userDao.findAll());
-        return "/user/list.jsp";
+        return jspView("/user/list.jsp");
     }
 }
--- board/src/main/java/next/controller/UpdateFormUserController.java
+++ board/src/main/java/next/controller/UpdateFormUserController.java
@@ -3,14 +3,17 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import core.mvc.AbstractController;
 import core.mvc.Controller;
+import core.mvc.JspView;
+import core.mvc.ModelAndView;
 import next.dao.UserDao;
 import next.model.User;
 
-public class UpdateFormUserController implements Controller {
+public class UpdateFormUserController extends AbstractController {
 
     @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         String userId = req.getParameter("userId");
         UserDao userDao = new UserDao();
         User user = userDao.findByUserId(userId);
@@ -18,6 +21,6 @@ public class UpdateFormUserController implements Controller {
             throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
         }
         req.setAttribute("user", user);
-        return "/user/updateForm.jsp";
+        return jspView("/user/updateForm.jsp");
     }
 }
